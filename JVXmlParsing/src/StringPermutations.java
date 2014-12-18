6e23ea20aa2934cd4d4344f0fd9eeaffc05d59fa
Mukul Kumar <mukul1613kumar@outlook.com>
2014-12-18 16:52:49 +0530

    Added code for permutations

--- JVXmlParsing/src/StringPermutations.java
+++ JVXmlParsing/src/StringPermutations.java
@@ -7,13 +7,35 @@ public class StringPermutations {
 
 	}
 
-	void permute() {
-        
+	void permute(int start, int last) {
+
+		if (start == last) {
+			for (String x : data)
+				System.out.print(x + " ");
+			System.out.println();
+		} else {
+
+			for (int i = start; i <= last; i++) {
+
+				swap(start, i);
+				permute(start + 1, last);
+				// System.out.println(tempArray);
+				swap(start,i);
+
+			}
+		}
+	}
+
+	void swap(int x, int y) {
+		String tt = data[x];
+		data[x] = data[y];
+		data[y] = tt;
 	}
 
 	public static void main(String[] args) {
 
 		StringPermutations mp = new StringPermutations();
-		mp.permute();
+		mp.permute(0, data.length - 1);
+
 	}
 }
