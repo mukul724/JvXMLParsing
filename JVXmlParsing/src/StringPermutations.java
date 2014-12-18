ddcc200d6bea23ddc00ca5de7352100b00c16f7d
Mukul Kumar <mukul1613kumar@outlook.com>
2014-12-18 12:12:14 +0530

    Addition of String Permutation class
    Change-Id: I1a44cba61df8666d42bae46f208096f34ce852d1
    Signed-off-by: Mukul Kumar <mukul1613kumar@outlook.com>

---
+++ JVXmlParsing/src/StringPermutations.java
@@ -0,0 +1,19 @@
+public class StringPermutations {
+
+	static String[] data = { "I1", "I2", "I3", "I4" };
+
+	public StringPermutations() {
+		// TODO Auto-generated constructor stub
+
+	}
+
+	void permute() {
+        
+	}
+
+	public static void main(String[] args) {
+
+		StringPermutations mp = new StringPermutations();
+		mp.permute();
+	}
+}
